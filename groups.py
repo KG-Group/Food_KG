e3825958e9382fc65e1fc1ca06326f1b73707e5e
MrVulcan <wkr1wkr@163.com>
2022-05-30 20:13:39 +0800

    食物分类后各区间统计个数

--- groups.py
+++ groups.py
@@ -11,12 +11,16 @@ def count(i):
 
 
 for i in items:
-    data = pd.read_excel('Normalized_.xlsx',sheet_name=i)
+    data = pd.read_excel('Normalized.xlsx',sheet_name=i)
     to = pd.DataFrame()
+    to_ = data
     res = pd.DataFrame()
     to = data.drop(data.columns[:4],axis=1)
     for j in range(len(to.columns)):
         count(j)
+    res.insert(column='样品名称',loc=0,value=to_['样品名称'])
+    res.insert(column='食品分类',loc=0,value=to_['食品分类'])
+    res.insert(column='样品编号',loc=0,value=to_['样品编号'])
     res.to_excel(excel_writer=writer,sheet_name=i,index=False)
     writer.save()
 writer.close()
