3ee8c364c6eaa701a91ce12e301ca599ac6d3104
MrVulcan <wkr1wkr@163.com>
2022-05-30 14:54:00 +0800

    foodEntities

--- foodEntities.py
+++ foodEntities.py
@@ -1,4 +1,5 @@
 from dataclasses import replace
+from tokenize import String
 from matplotlib.pyplot import axis
 import pandas as pd
 from py2neo import Graph,Node
@@ -9,4 +10,16 @@ graph = Graph('bolt://nas.boeing773er.site:7687')
 
 data = pd.read_excel("STaiwan.xlsx")
 data.fillna(value=0.0,inplace=True)
-print(data.iloc[1]) 
+
+def insert(index):
+    foodNode = Node('Food',name=data.iloc[index][2])
+    for i in range(len(data.columns)):
+        if isinstance (data.iloc[index][i],str):
+            foodNode[str(data.columns[i])] = data.iloc[index][i]
+        else:
+            foodNode[str(data.columns[i])] = float(data.iloc[index][i])
+    graph.create(foodNode)
+    graph.push(foodNode)
+
+for i in range(2120):
+    insert(i)
