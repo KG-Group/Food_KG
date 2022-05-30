f55c0452c2eebd3318a38d307cbc0a672b1328dd
MrVulcan <wkr1wkr@163.com>
2022-05-30 11:55:55 +0800

    导入食物实体

---
+++ foodEntities.py
@@ -0,0 +1,12 @@
+from dataclasses import replace
+from matplotlib.pyplot import axis
+import pandas as pd
+from py2neo import Graph,Node
+from yaml import ValueToken
+
+graph = Graph('bolt://nas.boeing773er.site:7687')
+
+
+data = pd.read_excel("STaiwan.xlsx")
+data.fillna(value=0.0,inplace=True)
+print(data.iloc[1])
